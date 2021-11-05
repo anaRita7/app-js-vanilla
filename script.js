0b3af48b4782f4b105907a24032d59a7e45d6179
anaRita7 <ana.cruz@ee.ufcg.edu.br>
2021-11-05 09:42:58 -0300

    anar - Adicionando novo elemnto à lista

--- script.js
+++ script.js
@@ -2,24 +2,34 @@ const transactionsUl = document.querySelector('#transactions');
 const inputTransactionName = document.querySelector('#text');
 const inputTransactionAmount= document.querySelector('#amount');
 
-const handleFormSubmit = event =>{
-
-    if(inputTransactionName.value.trim() === '' || 
-                inputTransactionAmount.value.trim() === ''){
-      alert('Informe a descrição e o valor da transação');
-
-    }
-}
-
-form.addEventListener('submit', handleFormSubmit);
-
 // objeto literal FICTICIO
-const dummyTransactions = [
+let dummyTransactions = [
     { id: 1, name: 'Bolo de brigadeiro', amount: -20 },
     { id: 2, name: 'Salario', amount: 300 },
     { id: 3, name: 'Torta de frango', amount: -10 },
     { id: 4, name: 'Violão', amount: 150 }
 ]
+
+const addTransactionInArray = (transactionName,transactionAmount) =>{
+    dummyTransactions.push({
+        id: 123,
+        name: transactionName.value,
+        amount: transactionAmount.value
+    })
+}
+
+const handleFormSubmit = event =>{
+    event.preventDefault();
+    if(inputTransactionName.value.trim() === '' || 
+                inputTransactionAmount.value.trim() === ''){
+      alert('Informe a descrição e o valor da transação');
+      return;
+    }
+    addTransactionInArray(inputTransactionName,inputTransactionAmount);
+    init();
+}
+
+form.addEventListener('submit', handleFormSubmit);
 
 const addTransactionIntoDOM = transaction =>{
     const li = document.createElement('li')
@@ -30,6 +40,7 @@ const addTransactionIntoDOM = transaction =>{
 }
 
 const init = () =>{
+    transactionsUl.innerHTML = '';
     dummyTransactions.forEach(addTransactionIntoDOM);
 }
 
